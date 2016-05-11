3a7b2d31abddb110ac1f6886a842008df58de298
Sebastian Reinhard <sebastian@sebbert.me>
2016-05-12 01:55:45 +0200

    Begin making room settings view

---
+++ src/components/adminView/roomSettingsView.js
@@ -0,0 +1,19 @@
+import React, {PropTypes} from 'react';
+
+import {
+	Checkbox,
+	Dropdown
+}
+
+export default class RoomSettingsView extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (<div>MyComponent</div>);
+	}
+}
+
+RoomSettingsView.propTypes = {
+};
