563f8f822315a8119adad3c91d84bc3b06ad309b
Sebastian Reinhard <sebastian@sebbert.me>
2016-08-20 14:41:31 +0200

    Updated firebase auth flow

--- src/actions/firebase.js
+++ src/actions/firebase.js
@@ -3,13 +3,15 @@ import { createFirebase } from '../helpers/firebase';
 import history from 'helpers/history';
 import { setAuthData, loginAndRedirect } from 'actions/session';
 
+import { app } from 'helpers/firebase';
+
+const auth = app.auth();
+
 export function initFirebase() {
 	return (dispatch, getState) => {
-		const ref = createFirebase();
-
-		ref.onAuth((authData) => {
+		auth.onAuthStateChanged((authData) => {
 			if(authData == null) {
-				ref.authAnonymously();
+				auth.signInAnonymously();
 			}
 
 			dispatch(setAuthData(authData));
