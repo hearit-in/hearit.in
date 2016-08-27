2466e8dc583aefc4d25a584966db966c1f8bd661
Sebastian Reinhard <sebastian@sebbert.me>
2016-08-27 17:27:33 +0200

    Better error reporting

--- src/actions/errors.js
+++ src/actions/errors.js
@@ -6,9 +6,14 @@ export const pushError = createAction(PUSH_ERROR);
 export const POP_ERROR = "POP_ERROR";
 export const popError = createAction(POP_ERROR);
 
-export function showError(message, seconds) {
+export function showError(error, seconds) {
 	seconds = seconds || 6;
 	return (dispatch, getState) => {
+		
+		let message = (error instanceof Error)
+			? error.message
+			: error;
+		
 		dispatch(pushError(message));
 
 		setTimeout(() => dispatch(popError()), seconds * 1000);
