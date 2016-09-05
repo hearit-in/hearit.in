c3d582ff5272e3cddc6444daf3081473efc5b302
Sebastian Reinhard <sebastian@sebbert.me>
2016-09-05 12:05:24 +0200

    Temp disable spotify reducer (no action)

--- src/reducers/session.js
+++ src/reducers/session.js
@@ -20,12 +20,12 @@ export const session = handleActions({
 	SET_AUTH_DATA:      (state, action) => state.set("authData", normalizeAuthData(action.payload)),
 	SET_IS_LOGGING_IN:  (state, action) => state.set("loggingIn", action.payload),
 	SET_IS_ADMIN:       (state, action) => state.set("isAdmin", action.payload),
-	SET_SPOTIFY_AUTH    (state, action) =>
+	/*SET_SPOTIFY_AUTH    (state, action) =>
 		state.set("spotifyAuth", {
 			authToken: action.payload.authToken,
 			refreshToken: action.payload.refreshToken,
 			id: action.payload.id
-		})
+		})*/
 }, new Map({
 	roomId: undefined,
 	authData: undefined,
